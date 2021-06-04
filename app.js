7b7c70bc879ccf4d865f83171e2e40d565dab0ff
Saidxon Sidiqov <jahongir780110@gmail.com>
2021-06-04 14:34:22 +0500

    added path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const compression = require("compression");
